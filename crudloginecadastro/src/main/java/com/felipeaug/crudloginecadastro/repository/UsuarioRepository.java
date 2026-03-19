0ab9cb555764509c16dda5e833bc916dc1b2cd0f
hzxfelps <felipe.augusto.gonzales1a@gmail.com>
2026-03-19 07:52:50 -0300

    Delete crudloginecadastro directory

--- crudloginecadastro/src/main/java/com/felipeaug/crudloginecadastro/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.felipeaug.crudloginecadastro.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.felipeaug.crudloginecadastro.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long > {
-    Usuario findByEmail(String email);
-}
