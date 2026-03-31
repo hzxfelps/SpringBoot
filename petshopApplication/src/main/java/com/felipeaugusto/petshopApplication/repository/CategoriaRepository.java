b40a49297f04eb852992e47ca1de34ceb6cd5c8e
hzxfelps <felipe.augusto.gonzales1a@gmail.com>
2026-03-31 13:06:04 -0300

    Delete petshopApplication directory

--- petshopApplication/src/main/java/com/felipeaugusto/petshopApplication/repository/CategoriaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.felipeaugusto.petshopApplication.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.felipeaugusto.petshopApplication.entity.Categoria;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long > {
-}
